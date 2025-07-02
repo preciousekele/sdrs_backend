ad47e4c417c8782d2e18a3b68bb70d6668028764
preciousekele <preshekele@gmail.com>
2025-07-02 02:53:05 +0100

    cascading user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post('/confirm-email', authController.confirmEmail);
+router.post('/confirm-email', authController.confirmEmail);
 router.get("/profile", verifyToken, (req, res) => {
     res.json({ message: "User profile", user: req.user }); //protected route
 });
